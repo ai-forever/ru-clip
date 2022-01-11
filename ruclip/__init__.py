a64676619c5268d027124c1c20a3e16bfddd3f2f
shonenkov <shonenkov@phystech.edu>
2022-01-11 10:27:16 +0300

    add models

--- ruclip/__init__.py
+++ ruclip/__init__.py
@@ -45,12 +45,6 @@ MODELS = {
             'bpe.model', 'config.json', 'pytorch_model.bin'
         ]
     ),
-    'ruclip-vit-large-patch14-384': dict(
-        repo_id='sberbank-ai/ruclip-vit-large-patch14-384',
-        filenames=[
-            'bpe.model', 'config.json', 'pytorch_model.bin'
-        ]
-    ),
 }
 
 
